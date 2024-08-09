831a79b2743cc09f5c90f0962b5afef4862f886d
kasif mansuri <kasif.mansuri.7@gmail.com>
2024-08-09 17:33:15 +0530

    add: organization detail page navbar and sidebar

--- app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { Accordion } from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useOrganization, useOrganizationList } from '@clerk/nextjs';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
 import { useLocalStorage } from 'usehooks-ts';
+import { NavItem, Organization } from './nav-item';
 
 interface SidebarProps {
   storageKey?: string;
@@ -12,11 +18,70 @@ export const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
     {}
   );
 
-  const { organization, isLoaded: isLoadedOrg } = useOrganization();
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
   const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
   });
-  return <div>side bar</div>;
+
+  console.log('expanded: ', expanded);
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({ ...curr, [id]: !expanded[id] }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span>Work Spaces</span>
+        <Button
+          asChild
+          size="icon"
+          type="button"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.length &&
+          userMemberships.data.map(({ organization }) => (
+            <NavItem
+              key={organization.id}
+              isActive={activeOrganization?.id === organization.id}
+              isExpanded={expanded[organization.id]}
+              organization={organization as Organization}
+              onExpand={onExpand}
+            />
+          ))}
+      </Accordion>
+    </>
+  );
 };
---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import {
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from '@/components/ui/accordion';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Activity, CreditCard, Settings, Layout } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter, usePathname } from 'next/navigation';
+
+interface NavItemsProps {
+  isActive: boolean;
+  isExpanded: boolean;
+  organization: Organization;
+  onExpand: (id: string) => void;
+}
+
+export type Organization = {
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
+
+export const NavItem = ({
+  isActive,
+  isExpanded,
+  organization,
+  onExpand,
+}: NavItemsProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const routes = [
+    {
+      label: 'Boards',
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: 'Activity',
+      icon: <Activity className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: 'Settings',
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+    {
+      label: 'Billing',
+      icon: <CreditCard className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/billing`,
+    },
+  ];
+
+  const onClick = (href: string) => {
+    router.push(href);
+  };
+
+  return (
+    <AccordionItem value={organization.id} className=" bottom-9">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          'flex items center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-700/90 transition text-start no-underline hover:no-underline ',
+          isActive && !isExpanded && 'bg-sky-500/10 text-sky-700'
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization?.imageUrl}
+              alt="Organization"
+              className="object-cover rounded-sm"
+            />
+          </div>
+          <span className="font-medium text-sm">{organization.name}</span>
+        </div>
+      </AccordionTrigger>
+
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => {
+          return (
+            <Button
+              key={route.label}
+              size="sm"
+              onClick={() => onClick(route.href)}
+              className={cn(
+                'w-full font-normal justify-start pl-10 mb-1',
+                pathname === route.href && 'bg-sky-500/10 text-sky-700'
+              )}
+              variant="ghost"
+            >
+              {route.icon}
+              {route.label}
+            </Button>
+          );
+        })}
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
